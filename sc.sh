2f1ac33a3de8cdec4c4207ad35677970d5364511
mfilitov <mfilitov@bk.ru>
2025-03-10 14:52:49 +0000

    TEST

---
+++ sc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Enter and create directory
+
+echo "Enter name Directory"
+read nameDirectory
+echo "You Directory - $nameDirectory"
+mkdir /home/script_bash/$nameDirectory
+
+
+#Create files
+
+
+for i in {1..10}
+do
+ touch /home/script_bash/$nameDirectory/File$i.txt
+done
+
+#Enter date in files
+
+for i in {1..10}
+do
+ date > /home/script_bash/$nameDirectory/File$i.txt
+done
